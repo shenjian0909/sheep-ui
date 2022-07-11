4238e197efba2671f23a65e0159304768feabe2c
shenjianAA <shenjian0909@163.com>
2022-07-11 08:36:37 +0800

    '去除无用代码'

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './index.scss'
-// import Button from './button'
-// import Space from './space'
 // 使用全量导出
 import SheepUI from './index'
 
